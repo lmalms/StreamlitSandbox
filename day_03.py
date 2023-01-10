5ef46f067e62dc1a245141fc544941435e335912
Lukas Malms <lukasmalms@Lukas-Malms.local>
2023-01-10 16:54:59 +0000

    day 03

---
+++ day_03.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
+
+st.header("st.write")
+
+# Example 1
+st.subheader("Display test")
+st.write("Hello *world!* :sunglasses:")
+
+# Example 2
+st.subheader("Display numbers")
+st.write(1234)
+
+# Example 3
+st.subheader("Display dataframes")
+df = pd.DataFrame(
+    data={
+        "first column": [1, 2, 3, 4],
+        "second column": [10, 20, 30, 40],
+    }
+)
+st.write(df)
+
+# Example 4
+st.subheader("Accept multiple arguments")
+st.write("Below is a dataframe", df, "Above is a dataframe.")
+
+# Example 5
+st.subheader("Display plots")
+df = pd.DataFrame(
+    data=np.random.randn(200, 3),
+    columns=["a", "b", "c"],
+)
+plot = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(
+        x="a",
+        y="b",
+        size="c",
+        color="c",
+        tooltip=["a", "b", "c"],
+    )
+)
+st.write(plot)
